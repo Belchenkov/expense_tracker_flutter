f967c0ffd7f2d6bcb43b855178fab24cedfb9df8
Belchenkov <u608110@gmail.com>
2024-08-03 10:45:14 +0400

    Delete expense from list from dismissible

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -22,6 +22,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+        isScrollControlled: true,
         context: context,
         builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
@@ -30,6 +31,12 @@ class _ExpensesState extends State<Expenses> {
   void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
     });
   }
 
@@ -51,7 +58,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The Chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense
+            ),
           ),
         ],
       ),
